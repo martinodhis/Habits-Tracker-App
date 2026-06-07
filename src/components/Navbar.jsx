324afbe70804637263a9acc5f0de79daac7b51dd
William Munene <williammunene2018@gmail.com>
2026-06-08 02:25:10 +0300

    added My Habits page to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,33 +9,30 @@ function Navbar() {
 
       <ul className="navbar_links">
         <li>
-          <NavLink
-            to="/"
-            end
+          <NavLink to="/" end
             className={({ isActive }) =>
-              isActive ? 'navbar_link navbar_link--active' : 'navbar_link'
-            }
-          >
+              isActive ? 'navbar_link navbar_link--active' : 'navbar_link'}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/add-habit"
+          <NavLink to="/habits"
             className={({ isActive }) =>
-              isActive ? 'navbar_link navbar_link--active' : 'navbar_link'
-            }
-          >
+              isActive ? 'navbar_link navbar_link--active' : 'navbar_link'}>
+            My Habits
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/add-habit"
+            className={({ isActive }) =>
+              isActive ? 'navbar_link navbar_link--active' : 'navbar_link'}>
             Add Habit
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/about"
+          <NavLink to="/about"
             className={({ isActive }) =>
-              isActive ? 'navbar_link navbar_link--active' : 'navbar_link'
-            }
-          >
+              isActive ? 'navbar_link navbar_link--active' : 'navbar_link'}>
             About
           </NavLink>
         </li>
@@ -45,4 +42,3 @@ function Navbar() {
 }
 
 export default Navbar
-
