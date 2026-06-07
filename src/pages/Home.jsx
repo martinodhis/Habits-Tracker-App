2136251efdbb28b0d65af8817070772e23e719b7
William Munene <williammunene2018@gmail.com>
2026-06-07 12:39:48 +0300

    basic pages structure, awaiting link to App

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <main>
+            <h1>Welcome To Habit Tracker</h1>
+            <p>Awaiting link to App.jsx</p>
+        </main>
+
+    )
+}
+
+export default Home;
